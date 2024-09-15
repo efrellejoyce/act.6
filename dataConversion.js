4476151c2b168b8cb728d3cf81df14a0065d8d2a
Efrelle Joyce Ducyao <efrellejoyced@gmail.com>
2024-09-15 13:00:05 +0700

    commit with dataConversion.js

---
+++ dataConversion.js
@@ -0,0 +1,26 @@
+//Convert a String to a Number 
+let price = "200"
+//Using parseInt()
+let num1 = parseInt(price);
+console.log(num1);
+//Using Number()
+let num2 = Number(price);
+console.log(num2)
+//Using the + operator
+let num3 = +price;
+console.log(num3);
+
+//Convert a Number to a string
+let distance = 150;
+//Using .toString()
+let str1 = distance.toString();
+console.log(str1);
+//Using string()
+let str2 = String(distance);
+console.log(str2);
+
+//Convert a String to a Decimal Number
+let weight = "72.5";
+//Using parseFloat()
+let decimalWeight = parseFloat(weight);
+console.log(decimalWeight);
